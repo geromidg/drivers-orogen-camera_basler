577e56dc9a252c5189a0366a805dcad342a9ab4e
Dimitris Geromichalos <geromidg@gmail.com>
2017-10-05 16:10:38 +0200

    Clean autogenerated files and link dependencies

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -1,54 +1,29 @@
-/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
-
 #include "Task.hpp"
 
-using namespace camera_basler;
+namespace camera_basler
+{
 
-Task::Task(std::string const& name)
-    : TaskBase(name)
-{
-}
+    Task::Task(std::string const& name) :
+        TaskBase(name)
+    {
+    }
 
-Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
-    : TaskBase(name, engine)
-{
-}
+    Task::Task(std::string const& name, RTT::ExecutionEngine* engine) :
+        TaskBase(name, engine)
+    {
+    }
 
-Task::~Task()
-{
-}
+    Task::~Task()
+    {
+    }
 
+    bool Task::configureHook()
+    {
+        if (! TaskBase::configureHook())
+            return false;
 
+        return true;
+    }
 
-/// The following lines are template definitions for the various state machine
-// hooks defined by Orocos::RTT. See Task.hpp for more detailed
-// documentation about them.
+}  // namespace camera_basler
 
-bool Task::configureHook()
-{
-    if (! TaskBase::configureHook())
-        return false;
-    return true;
-}
-bool Task::startHook()
-{
-    if (! TaskBase::startHook())
-        return false;
-    return true;
-}
-void Task::updateHook()
-{
-    TaskBase::updateHook();
-}
-void Task::errorHook()
-{
-    TaskBase::errorHook();
-}
-void Task::stopHook()
-{
-    TaskBase::stopHook();
-}
-void Task::cleanupHook()
-{
-    TaskBase::cleanupHook();
-}
--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -1,50 +1,33 @@
-/* Generated from orogen/lib/orogen/templates/tasks/Task.hpp */
-
-#ifndef CAMERA_BASLER_TASK_TASK_HPP
-#define CAMERA_BASLER_TASK_TASK_HPP
+#ifndef CAMERA_BASLER_TASK_HPP
+#define CAMERA_BASLER_TASK_HPP
 
 #include "camera_basler/TaskBase.hpp"
 
-namespace camera_basler {
+// TODO: Rename project to camera_basler_gige to avoid confilcts
+#include "camera_basler/CamGigEBasler.hpp"
 
-    /*! \class Task 
-     * \brief The task context provides and requires services. It uses an ExecutionEngine to perform its functions.
-     * Essential interfaces are operations, data flow ports and properties. These interfaces have been defined using the oroGen specification.
-     * In order to modify the interfaces you should (re)use oroGen and rely on the associated workflow.
-     * 
-     * \details
-     * The name of a TaskContext is primarily defined via:
-     \verbatim
-     deployment 'deployment_name'
-         task('custom_task_name','camera_basler::Task')
-     end
-     \endverbatim
-     *  It can be dynamically adapted when the deployment is called with a prefix argument. 
-     */
+namespace camera_basler
+{
+
     class Task : public TaskBase
     {
 	friend class TaskBase;
-    protected:
-
-
 
     public:
         /** TaskContext constructor for Task
          * \param name Name of the task. This name needs to be unique to make it identifiable via nameservices.
-         * \param initial_state The initial TaskState of the TaskContext. Default is Stopped state.
          */
         Task(std::string const& name = "camera_basler::Task");
 
         /** TaskContext constructor for Task 
          * \param name Name of the task. This name needs to be unique to make it identifiable for nameservices. 
          * \param engine The RTT Execution engine to be used for this task, which serialises the execution of all commands, programs, state machines and incoming events for a task. 
-         * 
          */
         Task(std::string const& name, RTT::ExecutionEngine* engine);
 
         /** Default deconstructor of Task
          */
-	~Task();
+	    ~Task();
 
         /** This hook is called by Orocos when the state machine transitions
          * from PreOperational to Stopped. If it returns false, then the
@@ -53,58 +36,11 @@ namespace camera_basler {
          *
          * It is meaningful only if the #needs_configuration has been specified
          * in the task context definition with (for example):
-         \verbatim
-         task_context "TaskName" do
-           needs_configuration
-           ...
-         end
-         \endverbatim
          */
         bool configureHook();
-
-        /** This hook is called by Orocos when the state machine transitions
-         * from Stopped to Running. If it returns false, then the component will
-         * stay in Stopped. Otherwise, it goes into Running and updateHook()
-         * will be called.
-         */
-        bool startHook();
-
-        /** This hook is called by Orocos when the component is in the Running
-         * state, at each activity step. Here, the activity gives the "ticks"
-         * when the hook should be called.
-         *
-         * The error(), exception() and fatal() calls, when called in this hook,
-         * allow to get into the associated RunTimeError, Exception and
-         * FatalError states. 
-         *
-         * In the first case, updateHook() is still called, and recover() allows
-         * you to go back into the Running state.  In the second case, the
-         * errorHook() will be called instead of updateHook(). In Exception, the
-         * component is stopped and recover() needs to be called before starting
-         * it again. Finally, FatalError cannot be recovered.
-         */
-        void updateHook();
-
-        /** This hook is called by Orocos when the component is in the
-         * RunTimeError state, at each activity step. See the discussion in
-         * updateHook() about triggering options.
-         *
-         * Call recover() to go back in the Runtime state.
-         */
-        void errorHook();
-
-        /** This hook is called by Orocos when the state machine transitions
-         * from Running to Stopped after stop() has been called.
-         */
-        void stopHook();
-
-        /** This hook is called by Orocos when the state machine transitions
-         * from Stopped to PreOperational, requiring the call to configureHook()
-         * before calling start() again.
-         */
-        void cleanupHook();
     };
-}
 
-#endif
+}  // namespace camera_basler
+
+#endif  // CAMERA_BASLER_TASK_HPP
 
